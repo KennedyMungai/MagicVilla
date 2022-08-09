1be02c4086e29962fe57a56eb0f92beb346c0aa5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 15:07:20 +0300

    Added Data Annotations to the Villa model

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -1,7 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace MagicVilla_VillaAPI.Models;
 
 public class Villa
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
+    [Required]
+    [MaxLength(30)]
     public string Name { get; set; } = string.Empty;
 }
