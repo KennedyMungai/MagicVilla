c974002eee938b8f23ce3a34b4d6ccfb13df6780
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 18:44:25 +0300

    Added the Logging service to the Program.cs

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,3 +1,5 @@
+using MagicVilla_VillaAPI.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,7 @@ builder.Services.AddControllers(options => {
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ILogging, Logging>();
 
 var app = builder.Build();
 
