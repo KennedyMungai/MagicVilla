da66a4cd76a222b5b702c9b76c7a4345fd068018
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 19:36:44 +0300

    Added seed data for the application

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -11,4 +11,23 @@ public class ApplicationDbContext : DbContext
     {
         
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Villa>()
+            .HasData(
+                new Villa()
+                {
+                    Id=1,
+                    Name="Kilimanjaro",
+                    Details="Pretty Cool and has a lot of floors",
+                    Rate=2000,
+                    Occupancy=2,
+                    Sqft=500,
+                    ImageUrl="https://dotnetmastery.com/bluevillaimages/villa3.jpg",
+                    Amenity="Hata sijui hii ni nini",
+                    CreatedDate = DateTime.Now
+                }
+            );
+    }
 }
