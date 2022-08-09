4249b4025bd98ca60679bc5f835ada76926db01a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 11:05:34 +0300

    Created a basic endpoint

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,3 +1,4 @@
+using MagicVilla_VillaAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
@@ -6,5 +7,11 @@ namespace MagicVilla_VillaAPI.Controllers;
 [Route("[controller]")]
 public class VillaAPIController : ControllerBase
 {
-    
+    public IEnumerable<Villa> GetVillas()
+    {
+        return new List<Villa> {
+            new Villa{Id = 1, Name = "Villa 1"},
+            new Villa{Id = 1, Name = "Villa 2"}
+        };
+    }
 }
