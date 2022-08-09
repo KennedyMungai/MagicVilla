14852eaab6275492c07f006c4714d0dd1a90dd54
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 18:06:41 +0300

    Commented out the line that penalizes the API when it uses any other format

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers(options => {
-    options.ReturnHttpNotAcceptable=true;
+    // options.ReturnHttpNotAcceptable=true;
 }).AddNewtonsoftJson()
     .AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
