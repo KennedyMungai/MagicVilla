faf9e369bfe6faa66d33a8eb70e35d3843a69ca3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 16:24:14 +0300

    Created a constructor for the DbContext class

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -6,4 +6,9 @@ namespace MagicVilla_VillaAPI.Data;
 public class ApplicationDbContext : DbContext
 {
     public DbSet<Villa>? Villas { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
 }
