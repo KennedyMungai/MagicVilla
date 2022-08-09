b39f6da75c0e33750c5b8fee846f97a2adb9b107
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 11:28:53 +0300

    Added the static List of Villas to the store

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -1,6 +1,12 @@
+using MagicVilla_VillaAPI.Models.Dto;
+
 namespace MagicVilla_VillaAPI.Data;
 
 public static class VillaStore
 {
-    
+    public static List<VillaDTO> villaList = new List<VillaDTO>
+    {
+        new VillaDTO{Id = 1, Name = "Villa 1", CreatedDate = DateTime.Now},
+        new VillaDTO{Id = 1, Name = "Villa 2", CreatedDate = DateTime.Now}
+    };
 }
