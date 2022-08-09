450453e517d9e1df36a86cf3be1886f80dfcc467
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 16:30:46 +0300

    Added the function to output Api data in the form of XML

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(options => {
     options.ReturnHttpNotAcceptable=true;
-}).AddNewtonsoftJson();
+}).AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
