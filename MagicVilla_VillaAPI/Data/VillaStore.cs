c317a3877249386fc01615cf22f346a2f8b4dbe5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 15:42:51 +0300

    Updated the data fields in the data store

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -6,7 +6,7 @@ public static class VillaStore
 {
     public static List<VillaDTO> villaList = new List<VillaDTO>
     {
-        new VillaDTO{Id = 1, Name = "Villa 1", CreatedDate = DateTime.Now},
-        new VillaDTO{Id = 2, Name = "Villa 2", CreatedDate = DateTime.Now}
+        new VillaDTO{Id = 1, Name = "Villa 1", Sqft= 100 ,Occupancy= 4},
+        new VillaDTO{Id = 2, Name = "Villa 2", Sqft= 100 ,Occupancy= 3}
     };
 }
