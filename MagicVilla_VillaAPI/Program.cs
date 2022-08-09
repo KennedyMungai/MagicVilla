f6695242b6bda4620c991868782a5eb1d912d47c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 16:21:59 +0300

    Added a comma at the end of the connection string

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>( option => 
     {
-        option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"))
+        option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
     }
 );
 
