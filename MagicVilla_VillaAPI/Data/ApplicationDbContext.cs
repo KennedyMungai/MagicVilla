04f927944d68124f68119244fd7b322e5e4966ff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 16:15:51 +0300

    Added the DbSet value for Villa

--- MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -1,8 +1,9 @@
+using MagicVilla_VillaAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    
+    public DbSet<Villa>? Villas { get; set; }
 }
