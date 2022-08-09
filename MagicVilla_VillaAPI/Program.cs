71fb5d0c4cd5563d2f427d41a26632420c51583a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 18:14:50 +0300

    Configured the logger

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,6 +1,13 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel
+                .Debug()
+                .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
 builder.Services.AddControllers(options => {
     // options.ReturnHttpNotAcceptable=true;
