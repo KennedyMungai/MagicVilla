2c729a0a9a83b54c3ac7ce6669c65766f3e12983
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 20:09:35 +0300

    Updated the fields

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -7,4 +7,9 @@ public class VillaDTO
     public int Occupancy { get; set; }
     public int Sqft { get; set; }
     public DateTime CreatedDate { get; set; }
+    public string Details { get; set; } = string.Empty;
+    public double Rate { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public string Amenity { get; set; } = string.Empty;
+    public DateTime UpdatedDate { get; set; }
 }
