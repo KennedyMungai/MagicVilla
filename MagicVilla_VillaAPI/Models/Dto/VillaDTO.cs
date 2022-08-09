7ea747773a3da1231010ccf45b9424698d36bcb1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 15:41:58 +0300

    Added the two extra fields

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -4,5 +4,7 @@ public class VillaDTO
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public int Occupancy { get; set; }
+    public int Sqft { get; set; }
     public DateTime CreatedDate { get; set; }
 }
