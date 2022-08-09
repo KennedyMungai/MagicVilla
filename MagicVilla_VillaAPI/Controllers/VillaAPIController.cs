200131c8c526e6886eb476295028f08b51aef09b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 11:09:41 +0300

    Added an attribute verb to tehe GetVillas function

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -7,6 +7,7 @@ namespace MagicVilla_VillaAPI.Controllers;
 [Route("api/VillaAPI")]
 public class VillaAPIController : ControllerBase
 {
+    [HttpGet]
     public IEnumerable<Villa> GetVillas()
     {
         return new List<Villa> {
