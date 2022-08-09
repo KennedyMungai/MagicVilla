26a87d0e53d5f27195705fc2454a0682d21e06f7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-08-09 16:06:21 +0300

    Added extra fields to the Villa model

--- MagicVilla_VillaAPI/Models/Villa.cs
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -11,6 +11,12 @@ public class Villa
     [Required(ErrorMessage = "You have to have a name")]
     [MaxLength(30)]
     public string Name { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+    public double Rate { get; set; }
     public int Occupancy { get; set; }
     public int Sqft { get; set; }
+    public string ImageUrl { get; set; } = String.Empty;
+    public string Amenity { get; set; } = String.Empty;
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
 }
